cb38d4a65d4841d62a0644a458197cebea20c9c7
Пименов Александр Витальевич <a.pimenov@avtodor-mc.ru>
2022-01-20 12:40:25 +0300

    Добавлены urls и view-функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse('Главная страница')
+
+
+def group_posts(request, slug):
+    return HttpResponse(f'Для Вас мы сгруппировали посты здесь: {slug}')
+
